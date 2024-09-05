b3ddd7c1319cbc48628b89d3f1d0b4343f7d9271
VyacheslavVolkhin <vv@htmlworker.com>
2024-09-05 13:37:44 +0500

    add

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,30 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+// filter actions
+const filterButtonOpen = document.querySelector('.js-filter-open');
+const filterButtonClose = document.querySelector('.js-filter-close');
+const filterButtonCloseHeader = document.querySelector('.js-filter-close-header');
+if (filterButtonOpen) {
+  filterButtonOpen.addEventListener("click", function (event) {
+    document.body.classList.add("filter-show");
+    event.preventDefault();
+  });
+}
+if (filterButtonClose) {
+  filterButtonClose.addEventListener("click", function (event) {
+    document.body.classList.remove("filter-show");
+    event.preventDefault();
+  });
+}
+if (filterButtonCloseHeader) {
+  filterButtonCloseHeader.addEventListener("click", function (event) {
+    document.body.classList.remove("filter-show");
+    event.preventDefault();
+  });
+}
+
+
 
 //form input clear
 const inputFields = document.querySelectorAll(
@@ -174,7 +198,6 @@ const wrapWidth = document.querySelector(".wrap").offsetWidth;
 const bodyElem = document.querySelector("body");
 function popupElementsClear() {
   document.body.classList.remove("menu-show");
-  document.body.classList.remove("filter-show");
   document.body.classList.remove("search-show");
   popupElements.forEach((element) => element.classList.remove("popup-right"));
 }
@@ -210,9 +233,6 @@ for (i = 0; i < togglePopupButtons.length; i++) {
       }
       if (this.closest(".popup-search-wrap")) {
         document.body.classList.add("search-show");
-      }
-      if (this.closest(".popup-filter-wrap")) {
-        document.body.classList.add("filter-show");
       }
       popupElementsContentPositionClass();
     }
@@ -282,6 +302,57 @@ popupElements.forEach((element) => {
     }
   }
 });
+
+
+//js tabs
+const tabsNav = document.querySelectorAll('.js-tabs-nav')
+const tabsBlocks = document.querySelectorAll('.js-tab-block')
+const tabsButtonTitle = document.querySelectorAll('.js-tab-title')
+const tabsButtonContent = document.querySelectorAll('.js-tab-content')
+function tabsActiveStart() {
+	for (iTab = 0; iTab < tabsBlocks.length; iTab++) {
+		if (tabsBlocks[iTab].classList.contains('active')) {
+			tabsBlocks[iTab].classList.remove('active')
+		}
+	}
+	for (i = 0; i < tabsNav.length; i++) {
+		let tabsNavElements = tabsNav[i].querySelectorAll('[data-tab]')
+		for (iElements = 0; iElements < tabsNavElements.length; iElements++) {
+			if (tabsNavElements[iElements].classList.contains('active')) {
+				let tabsNavElementActive = tabsNavElements[iElements].dataset.tab
+				for (j = 0; j < tabsBlocks.length; j++) {
+					if (tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive) > -1) {
+						console.log(tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive))
+						tabsBlocks[j].classList.add('active')
+					}
+				}
+			}
+		}
+	}
+	
+}
+for (i = 0; i < tabsButtonTitle.length; i++) {
+	tabsButtonTitle[i].addEventListener('click', function (e) {
+		this.classList.toggle('active')
+		e.preventDefault()
+		e.stopPropagation()
+		return false
+	})
+}
+for (i = 0; i < tabsNav.length; i++) {
+	tabsNav[i].addEventListener('click', function (e) {
+		if (e.target.closest('[data-tab]')) {
+			let tabsNavElements = this.querySelector('[data-tab].active')
+			tabsNavElements ? tabsNavElements.classList.remove('active') : false
+			e.target.closest('[data-tab]').classList.add('active')
+			tabsActiveStart()
+			e.preventDefault()
+			e.stopPropagation()
+			return false
+		}
+	})
+}
+tabsActiveStart()
 
 // Popups
 let popupCurrent;
@@ -430,4 +501,39 @@ const swiperSliderPhoto = new Swiper(".slider-photo .swiper", {
     nextEl: ".btn-action-ico.ico-arrow.ico-arrow-next.button-slider-photo-next",
     prevEl: ".btn-action-ico.ico-arrow.ico-arrow-prev.button-slider-photo-prev",
   },
+});
+
+
+
+//slider media thumbs preview
+const swiperMediaPreview = new Swiper(".slider-media-thumbs .swiper",
+{
+  loop: false,
+  slidesPerView: 5,
+  spaceBetween: 0,
+  threshold: 5,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  freeMode: false,
+  navigation: false,
+});
+
+//slider media thumbs main
+const swiperMediaMain = new Swiper(".slider-media-main .swiper",
+{
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  autoHeight: true,
+  speed: 400,
+  threshold: 5,
+  freeMode: false,
+  watchSlidesProgress: true,
+  navigation: false,
+  pagination: {
+	clickable: true,
+	},
+  thumbs: {
+	swiper: swiperMediaPreview,
+	},
 });
